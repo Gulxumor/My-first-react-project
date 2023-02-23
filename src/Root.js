c5151a74ecad53cb1d29742fd2db53703c758cca
Gulxumor <gulxumorerkinjonova2005@gmail.com>
2023-02-23 14:22:03 +0500

    continuing

--- src/Root.js
+++ src/Root.js
@@ -1,19 +1,10 @@
 import React from 'react'
 import './Root.css'
-import Navbar from './Navbar.js'
-import Header from './Header.js'
-import Newsletter from './Newsletter.js'
-import Card from './Card'
-
-
 
 function Root() {
     return (
     <div>
-        <Navbar />
-        <Header />
-        <Card />
-        <Newsletter />
+        
     </div>
     )
 }
